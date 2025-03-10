a8fd30d0b5de3719b9b807a2ef398b46fb7d3880
Ivan Vorobev <ivan.vorobjov@gmail.com>
2025-03-10 16:46:08 +0200

    Scraping with pagination

--- divanpars/divanpars/spiders/divannewpars.py
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -16,10 +16,24 @@ class DivannewparsSpider(scrapy.Spider):
         self.start_urls = [url[0] for url in self.divan.categories]
         self.category_names = [url[1] for url in self.divan.categories]
         self.driver.quit()
+        # pagination  (//a[@data-testid='item' and contains(@href, '/category/divany-i-kresla/') and contains(@class,"PaginationLink")])[last()]
 
     def parse(self, response):
-        cards =  response.xpath('//div[@itemscope and @itemprop="itemListElement" and @data-testid="product-card"]')
+        print(f"Обработка ********************* {response.url}")
+        if "/page-" not in response.url:
+            last_page = response.xpath(
+                "(//a[@data-testid='item' and contains(@class,'PaginationLink')])[last()]/text()").get()
 
+            if last_page:
+                last_page = int(last_page)  # Преобразуем в число
+                category_url = response.url  # Базовый URL категории
+
+                # Создаем запросы только для страниц от 2 до последней
+                for page in range(2, last_page + 1):
+                    next_page_url = f"{category_url}/page-{page}"
+                    yield scrapy.Request(url=next_page_url, callback=self.parse)
+
+        cards = response.xpath('//div[@itemscope and @itemprop="itemListElement" and @data-testid="product-card"]')
         for card in cards:
             card_name = card.xpath('(.//span[@itemprop="name"])/text()').get()
             price = card.xpath('(.//span[@data-testid="price"])[2]/text()').get()
